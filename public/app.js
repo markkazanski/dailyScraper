adc5093e20905d22d9a035c505edcc927bbf90d6
Mark Kazanski <markkazanski@gmail.com>
2018-02-16 23:23:40 -0600

    bootstrap

--- public/app.js
+++ public/app.js
@@ -1,15 +1,18 @@
-// Grab the articles as a json
-$.getJSON("/articles", function(data) {
-  // For each one
-  for (var i = 0; i < data.length; i++) {
-    // Display the apropos information on the page
-    $("#articles").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].link + "<br />" + data[i].summary + "</p>");
-  }
+
+$(document).ready(function(){
+  // Grab the articles as a json
+  $.getJSON("/articles", function(data) {
+    // For each one
+    for (var i = 0; i < data.length; i++) {
+      // Display the apropos information on the page
+      $("#articles").append("<div class='article' data-id='" + data[i]._id + "'><h3>" + data[i].title + "</h3><p><strong>" + data[i].link + "</strong></p><p>" + data[i].summary + "</p></div>");
+    }
+  });
 });
 
 
 // Whenever someone clicks a p tag
-$(document).on("click", "p", function() {
+$(document).on("click", "div.article", function() {
   // Empty the notes from the note section
   $("#notes").empty();
   // Save the id from the p tag
@@ -26,12 +29,12 @@ $(document).on("click", "p", function() {
       // The title of the article
       $("#notes").append("<h2>" + data.title + "</h2>");
       // An input to enter a new title
-      $("#notes").append("<input id='titleinput' name='title' >");
+      $("#notes").append("<label>Note Title</label><input class='form-control' id='titleinput' name='title' >");
       // A textarea to add a new note body
-      $("#notes").append("<textarea id='bodyinput' name='body'></textarea>");
+      $("#notes").append("<label>Note Body</label><textarea class='form-control' id='bodyinput' name='body'></textarea>");
       // A button to submit a new note, with the id of the article saved to it
-      $("#notes").append("<button data-id='" + data._id + "' id='savenote'>Save Note</button>");
-      $("#notes").append(`<a id="deletenote" href="#" data-article-id="${data._id}">Delete</a>`);
+      $("#notes").append("<button class='btn btn-primary' data-id='" + data._id + "' id='savenote'>Save Note</button>");
+      $("#notes").append(` &nbsp; <button id="deletenote" href="#" data-article-id="${data._id}">Delete</button>`);
       // If there's a note in the article
       if (data.note) {
         // Place the title of the note in the title input
