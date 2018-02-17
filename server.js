4e95b7c46e4ef9ac15c36bef841a558377cf1c9b
Mark Kazanski <markkazanski@gmail.com>
2018-02-16 23:48:27 -0600

    heroku

--- server.js
+++ server.js
@@ -29,7 +29,8 @@ app.use(express.static("public"));
 // By default mongoose uses callbacks for async queries, we're setting it to use promises (.then syntax) instead
 // Connect to the Mongo DB
 mongoose.Promise = Promise;
-mongoose.connect("mongodb://localhost/dailyscraper", {
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/dailyscraper";
+mongoose.connect(MONGODB_URI, {
   useMongoClient: true
 });
 
